e9eeb3c34c3586d947c51bd50758552e79dcd4fe
Miljenko Korpar <miljenko.korpar@ibm.com>
2022-06-17 10:28:55 +0200

    Init data for DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
-INSERT INTO Bike VALUES (1, 'b1', 'brand1', 100.00, 'red');
-INSERT INTO Bike VALUES (2, 'b2', 'brand1', 200.00, 'blue');
-INSERT INTO Bike VALUES (3, 'b3', 'brand1', 300.00, 'yellow');
-INSERT INTO Bike VALUES (4, 'b4', 'brand1', 400.00, 'green');
+INSERT INTO Bike (name, brand, price, color) VALUES
+('Aspect 960', 'Scott', 5699, 'gray'),
+('Picton I', 'Head', 8999, 'yellow'),
+('Legos II', 'Head', 20799.96, 'green'),
+('Marlin 8', 'Trek', 7799, 'red'),
+('Marlin 6', 'Trek', 5399, 'factory orange'),
+('Traverse 1.5', 'Fuji', 4999.99, 'blue'),
+('Feather', 'Fuji', 5000, 'red'),
+('Mankind NXS', 'BMX', 3519.99, 'gray'),
+('STY STR', 'BMX', 3699.89, 'black'),
+('Nevada 26 1.9 V-brake', 'Fuji', 3299.99, 'black');
