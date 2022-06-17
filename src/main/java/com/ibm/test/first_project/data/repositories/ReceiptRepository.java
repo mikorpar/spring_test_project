9c51a40b980ec603b533a9c6bb9535132b134771
Miljenko Korpar <miljenko.korpar@ibm.com>
2022-06-17 16:40:07 +0200

    Initial receipt controller, service and repository implementation

--- src/main/java/com/ibm/test/first_project/data/repositories/ReceiptRepository.java
+++ src/main/java/com/ibm/test/first_project/data/repositories/ReceiptRepository.java
@@ -1,2 +1,8 @@
-package com.ibm.test.first_project.data.repositories;public interface ReceiptRepository {
+package com.ibm.test.first_project.data.repositories;
+
+import com.ibm.test.first_project.data.models.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+
 }
--- src/main/java/com/ibm/test/first_project/services/ReceiptService.java
+++ src/main/java/com/ibm/test/first_project/services/ReceiptService.java
@@ -1,2 +1,47 @@
-package com.ibm.test.first_project.services;public class ReceiptService {
+package com.ibm.test.first_project.services;
+
+import com.ibm.test.first_project.data.dtos.ItemResDTO;
+import com.ibm.test.first_project.data.dtos.ReceiptBikePostReqDTO;
+import com.ibm.test.first_project.data.dtos.ReceiptGetResDTO;
+import com.ibm.test.first_project.data.dtos.ReceiptPostReqDTO;
+import com.ibm.test.first_project.data.repositories.ReceiptRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReceiptService {
+
+    private final ReceiptRepository receiptRepository;
+
+    public ReceiptPostReqDTO storeReceipt(List<ReceiptBikePostReqDTO> bikes) {
+        return null;
+    }
+
+    public List<ReceiptGetResDTO> getReceipts() {
+        List<ReceiptGetResDTO> result = new ArrayList<>();
+
+        receiptRepository.findAll().forEach(receipt -> {
+            ReceiptGetResDTO receiptDTO = new ReceiptGetResDTO();
+            receiptDTO.setId(receipt.getId());
+            receiptDTO.setCreatedAt(receipt.getCreatedAt());
+
+            ArrayList<ItemResDTO> itemsDTO = new ArrayList<>();
+            receipt.getItems().forEach(item -> {
+               ItemResDTO itemDTO = new ItemResDTO();
+               itemDTO.setBike(item.getBike());
+               itemDTO.setQuantity(item.getQuantity());
+               itemsDTO.add(itemDTO);
+            });
+
+            receiptDTO.setItems(itemsDTO);
+            receiptDTO.setTotalPrice(receipt.getTotalPrice());
+            result.add(receiptDTO);
+        });
+
+        return result;
+    }
 }
