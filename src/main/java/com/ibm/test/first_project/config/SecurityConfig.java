0df8d3282f24769f47fd7fbf10f991e0dd970e33
mikorpar <miljenko.korpar@gmail.com>
2022-06-20 16:36:15 +0200

    Added Basic Authentication

--- src/main/java/com/ibm/test/first_project/config/SecurityConfig.java
+++ src/main/java/com/ibm/test/first_project/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +17,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/login", "/error", "/h2-console/**").permitAll()
-                .and().csrf().ignoringAntMatchers("/h2-console/**")
+        http.authorizeRequests().antMatchers("/login", "/error", "/h2-console/**").permitAll()
+                .and().authorizeRequests().anyRequest().authenticated().and().httpBasic()
+                .and().csrf().ignoringAntMatchers("/login", "/h2-console/**")
                 .and().csrf().ignoringAntMatchers("/bikes/**", "/orders/**") // TODO delete after security part is implemented
                 .and().headers().frameOptions().sameOrigin();
+
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .passwordEncoder(passwordEncoder()::encode)
+                .username("user")
+                .password("passwd")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
