052906abe58be3311eadcfa5d929241ed0b0d404
mikorpar <miljenko.korpar@gmail.com>
2022-06-20 12:11:08 +0200

    Code refactoring

--- src/main/java/com/ibm/test/first_project/services/StatisticsService.java
+++ src/main/java/com/ibm/test/first_project/services/StatisticsService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class StatisticsService {
@@ -16,23 +18,23 @@ public class StatisticsService {
     private final SalesOrderService salesOrderService;
     private final BikeService bikeService;
 
-    // TODO statistika prodaje bicikala, servis koji vraća ukupnu vrijednost svih prodanih bicikala
-    //  te postotak cijene u ukupnoj cijeni svih prodanih bicikala za tekucu godinu po marki, boji
-    private BigDecimal calcTotalSalesIncome() {
-        BigDecimal totalIncome = BigDecimal.valueOf(0);
-
-        for (SalesOrder salesOrder : salesOrderService.getOrders()) {
-            totalIncome = totalIncome.add(salesOrder.getTotalPrice());
-        }
-
-        return totalIncome;
+    public StatisticsGetRes calcTotalSalesIncomeByBrand(String brand) {
+        return calcTotalSalesIncomeByBikeProperty(bikeService.getAllBikesByBrand(brand));
     }
 
-    public StatisticsGetRes calcTotalSalesIncomeByBrand(String brand) {
+    public StatisticsGetRes calcTotalSalesIncomeByColor(String color) {
+        return calcTotalSalesIncomeByBikeProperty(bikeService.getAllBikesByColor(color));
+    }
+
+    public StatisticsGetRes calcTotalSalesIncomeByBrandAndColor(String brand, String color) {
+        return calcTotalSalesIncomeByBikeProperty(bikeService.getAllBikesByBrandAndColor(brand, color));
+    }
+
+    private StatisticsGetRes calcTotalSalesIncomeByBikeProperty(List<Bike> bikes) {
         BigDecimal totalIncome = calcTotalSalesIncome();
         BigDecimal totalIncomeByBrand = BigDecimal.valueOf(0);
 
-        for (Bike bike : bikeService.getAllBikesByBrand(brand)) {
+        for (Bike bike : bikes) {
             for (OrderItem item : bike.getItems()) {
                 totalIncomeByBrand = totalIncomeByBrand.add(item.getBike().getPrice().multiply(BigDecimal.valueOf(item.getQuantity())));
             }
@@ -45,37 +47,13 @@ public class StatisticsService {
         return stastistics;
     }
 
-    public StatisticsGetRes calcTotalSalesIncomeByColor(String color) {
-        BigDecimal totalIncome = calcTotalSalesIncome();
-        BigDecimal totalIncomeByColor = BigDecimal.valueOf(0);
+    private BigDecimal calcTotalSalesIncome() {
+        BigDecimal totalIncome = BigDecimal.valueOf(0);
 
-        for (Bike bike : bikeService.getAllBikesByColor(color)) {
-            for (OrderItem item : bike.getItems()) {
-                totalIncomeByColor = totalIncomeByColor.add(item.getBike().getPrice().multiply(BigDecimal.valueOf(item.getQuantity())));
-            }
+        for (SalesOrder salesOrder : salesOrderService.getOrders()) {
+            totalIncome = totalIncome.add(salesOrder.getTotalPrice());
         }
 
-        StatisticsGetRes stastistics = new StatisticsGetRes();
-        stastistics.setTotalIncome(totalIncome);
-        stastistics.setPercentage(totalIncomeByColor.divide(totalIncome, 2, RoundingMode.HALF_UP).doubleValue() * 100);
-
-        return stastistics;
-    }
-
-    public StatisticsGetRes calcTotalSalesIncomeByBrandAndColor(String brand, String color) {
-        BigDecimal totalIncome = calcTotalSalesIncome();
-        BigDecimal totalIncomeByBrandAndColor = BigDecimal.valueOf(0);
-
-        for (Bike bike : bikeService.getAllBikesByBrandAndColor(brand, color)) {
-            for (OrderItem item : bike.getItems()) {
-                totalIncomeByBrandAndColor = totalIncomeByBrandAndColor.add(item.getBike().getPrice().multiply(BigDecimal.valueOf(item.getQuantity())));
-            }
-        }
-
-        StatisticsGetRes stastistics = new StatisticsGetRes();
-        stastistics.setTotalIncome(totalIncome);
-        stastistics.setPercentage(totalIncomeByBrandAndColor.divide(totalIncome, 2, RoundingMode.HALF_UP).doubleValue() * 100);
-
-        return stastistics;
+        return totalIncome;
     }
 }
