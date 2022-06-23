93af9f0c9611a6fc6744b9c2ab39f68ed8daa43f
mikorpar <miljenko.korpar@gmail.com>
2022-06-23 15:03:18 +0200

    Removed unused class

--- src/main/java/com/ibm/test/first_project/data/specifications/BikeSpecification.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.test.first_project.data.specifications;
-
-import com.ibm.test.first_project.data.models.Bike;
-import org.springframework.data.jpa.domain.Specification;
-
-public class BikeSpecification {
-
-
-}
