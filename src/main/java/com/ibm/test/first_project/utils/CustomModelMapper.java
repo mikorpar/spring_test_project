0b2eedb73af0cefd0bc3e29a879e75c8978dbfa9
mikorpar <miljenko.korpar@gmail.com>
2022-06-23 10:14:33 +0200

    Added custom mapping for maping bike and bike DTOs

--- src/main/java/com/ibm/test/first_project/utils/CustomModelMapper.java
+++ src/main/java/com/ibm/test/first_project/utils/CustomModelMapper.java
@@ -1,5 +1,11 @@
 package com.ibm.test.first_project.utils;
 
+import com.ibm.test.first_project.data.dtos.bike.BikeGetResDTO;
+import com.ibm.test.first_project.data.dtos.bike.BikeUpdateResDTO;
+import com.ibm.test.first_project.data.dtos.sales_order.SalesOrderPostResDTO;
+import com.ibm.test.first_project.data.models.Bike;
+import com.ibm.test.first_project.data.models.OrderItem;
+import com.ibm.test.first_project.data.models.SalesOrder;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +14,30 @@ import java.util.stream.Collectors;
 
 @Component
 public class CustomModelMapper extends ModelMapper {
+    public CustomModelMapper() {
+        customMapping();
+    }
 
     public <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
         return source
                 .stream()
                 .map(elem -> this.map(elem, targetClass))
                 .collect(Collectors.toList());
+    }
+
+    private void customMapping() {
+        this.typeMap(Bike.class, BikeGetResDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getBrand().getName(),
+                    BikeGetResDTO::setBrand);
+            mapper.map(src -> src.getColor().getName(),
+                    BikeGetResDTO::setColor);
+        });
+
+        this.typeMap(Bike.class, BikeUpdateResDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getBrand().getName(),
+                    BikeUpdateResDTO::setBrand);
+            mapper.map(src -> src.getColor().getName(),
+                    BikeUpdateResDTO::setColor);
+        });
     }
 }
