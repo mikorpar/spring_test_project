e26917664dac3da8161901bf4e215d78101f1a51
mikorpar <miljenko.korpar@gmail.com>
2023-02-18 22:08:38 +0100

    Remove unused annotation

--- src/main/java/com/ibm/test/first_project/utils/CustomModelMapper.java
+++ src/main/java/com/ibm/test/first_project/utils/CustomModelMapper.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-@RequiredArgsConstructor
 public class CustomModelMapper extends ModelMapper {
 
     public CustomModelMapper() {
